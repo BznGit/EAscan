2371649ea133dfc40ffa9ad134ac8ae5d2ff1770
BznGit <bez-al@mail.ru>
2023-12-18 15:27:17 +0300

    as

--- appScan.js
+++ appScan.js
@@ -2,4 +2,4 @@ const apiScanStarter = require('./backend/src/apiScan');
 const MainApp = require('./backend/src/server');
 const mainApp= new MainApp();
 mainApp.start();
-apiScanStarter();
+apiScanStarter(); // запуск сканирования
